aead308ee81c4a194721a9240da65fb215938788
SPACED-MAN <i@spaced-man.com>
2024-10-20 14:11:41 -0400

    Add override handling, better reflect v3 core field examples

--- index.ts
+++ index.ts
@@ -1,19 +1,33 @@
+import type { TextField } from "payload";
+
+type ColorSwatchFieldType = (overrides?: Partial<TextField>) => TextField;
+
 // TO DO: Add Cell support for column sorting by color
-export const colorSwatchField = {
-  name: "color",
-  type: "text",
-  validate: (value) =>
-    value == null ||
-    value?.match(/^#(?:[0-9a-fA-F]{3}){1,2}$/)?.length === 1 ||
-    "This field is required",
-  required: false,
-  admin: {
-    position: "sidebar",
-    components: {
-      Field: {
-        path: "@modernaut/payload-color-swatch-field/ColorSwatchComponent#ColorSwatchComponent",
-        // path: '@/fields/colorswatch/ColorSwatchComponent#ColorSwatchComponent',  // Comment out above if you'd rather place this in /fields directory of your app
+export const colorSwatchField: ColorSwatchFieldType = (overrides = {}) => {
+  // Expect ts error here because of typescript mismatching Partial<TextField> with TextField
+  // @ts-expect-error
+  const colorSwatchField: TextField = {
+    name: "color",
+    type: "text",
+    index: true,
+    label: "Color Swatch",
+    required: false,
+    validate: (value) =>
+      value == null ||
+      value?.match(/^#(?:[0-9a-fA-F]{3}){1,2}$/)?.length === 1 ||
+      "This field is required",
+    ...(overrides || {}),
+    admin: {
+      position: "sidebar",
+      ...(overrides?.admin || {}),
+      components: {
+        Field: {
+          path: "@modernaut/payload-color-swatch-field/ColorSwatchComponent#ColorSwatchComponent",
+          // path: '@/fields/colorswatch/ColorSwatchComponent#ColorSwatchComponent', // Replace above with this line if you'd rather place this source in /fields directory of your app
+        },
       },
     },
-  },
+  };
+
+  return colorSwatchField;
 };
